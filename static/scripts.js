802a26fd2fd72f6cf87266adfedc9c155bfb2696
eagraf <graf.ethan@gmail.com>
2017-09-30 02:14:43 -0400

    Basic integration of google maps.

--- static/scripts.js
+++ static/scripts.js
@@ -2,3 +2,12 @@ $(window).on('load',function(){
     $('#profile-modal').modal('show');
 });
 
+function initMap() {
+    const bedford = { lat: 42.490556, lng: -71.276667 };
+    let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: bedford
+    });
+}
+    
+
