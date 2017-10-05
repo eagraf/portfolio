ae45d9bd33e70998d606c75738dcdab97e223e10
eagraf <graf.ethan@gmail.com>
2017-10-05 00:16:10 -0400

    Fixed bug where navbar pushes map down.

--- static/scripts.js
+++ static/scripts.js
@@ -5,7 +5,7 @@ $(window).on('load',function(){
 function initMap() {
     const bedford = { lat: 42.490556, lng: -71.276667 };
     let map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 8,
         center: bedford
     });
 }
